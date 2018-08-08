c212cdfe4173023862d4cfd647e5a8c6bae4a47f
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-08-08 23:14:21 +0200

    add Router to app

---
+++ src/components/Router.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from './app';
+import AdminPanel from "./adminPanel";
+import PageNotFound from "./pageNotFound";
+
+class Router extends React.Component{
+
+    constructor( props ) {
+        super( props );
+
+    };
+
+    render(){
+        return(
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={App}/>
+                    <Route path="/admin" component={AdminPanel}/>
+                    <Route component={PageNotFound}/>
+                </Switch>
+            </BrowserRouter>
+        );
+    };
+
+}
+
+export default Router;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app';
+import Router from "./components/Router";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
