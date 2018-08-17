27bacbf0eb903f01efb9d9085e8f4ea54d32c293
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-08-17 18:45:49 +0200

    description over the entire width

--- src/components/bookView.jsx
+++ src/components/bookView.jsx
@@ -9,17 +9,17 @@ class BookView extends React.Component{
     render(){
         return(
             <div className="row bookView">
-                <div className="col-xs-3">
+                <div className="col-6">
                     <b>Name:{this.props.book.name} </b>
                 </div>
-                <div className="col-xs-3">
+                <div className="col-6">
                     <i>Author:{this.props.book.author} </i>
                 </div>
 
-                <div className="col-xs-3">
+                <div className="col-12">
                     <i>Description:{this.props.book.description}</i>
                 </div>
-                <div className="col-xs-3">
+                <div className="col-6">
                     <button
                         className='btn btn-info'
                         onClick={ () => this.props.addToOrder(this.props.book) }>
