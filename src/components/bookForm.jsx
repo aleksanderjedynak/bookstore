4e2292c33955ee5eea46507647b2e9c58603754b
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-09-23 21:07:14 +0200

    refactoring bookForm

--- src/components/bookForm.jsx
+++ src/components/bookForm.jsx
@@ -1,70 +1,154 @@
 import React from 'react';
+import { fbase } from "../fbase";
 
 class BookForm extends React.Component{
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            book: {
+                name: "",
+                author: "",
+                description: "",
+                bookOnStock: false,
+                image: "",
+                select: 2,
+            },
+            books: [],
+
+        };
+    };
+
+    componentDidMount(){
+        this.ref = fbase.syncState(
+            'bookstore/books',
+            {
+                context: this,
+                state: 'books',
+            }
+        );
+    };
+
+    componentWillUnmount(){
+        fbase.removeBinding(this.ref);
+    };
+
+    handleChangeSelect = (event) =>{
+        let newBook = {
+            ...this.state.book,
+            [event.target.name]: event.target.value,
+        };
+        this.setState({
+            book: newBook,
+        });
+    };
+
+    handleAddNewBook = (event) => {
+
+        event.preventDefault();
+        let newBook = { ...this.state.book };
+        this.setState({
+            books: [...this.state.books, newBook],
+            book: {
+                name: "",
+                author: "",
+                description: "",
+                bookOnStock: false,
+                image: "",
+                select: 2,
+            },
+        });
+    };
+
+    handleChange = (event) => {
+        let newBook;
+
+        if(event.target.name === "bookOnStock"){
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.checked,
+            };
+        } else{
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.value,
+            };
+        }
+
+        this.setState({
+            book: newBook,
+        });
+    };
+
     render() {
         return (
-            <div className='row justify-content-center'>
-                <div className='col-4'>
-                    <form onSubmit={(event) => this.props.handleAddNewBook(event)}>
+            <div className='row'>
+                <div className='col-6'>
+                    <h1>List Book</h1>
+
+                </div>
+                <div className='col-6'>
+                    <h1>New Book</h1>
+                    <form onSubmit={(event) => this.handleAddNewBook(event)}>
                         <div className='form-group'>
                             <input
-                                value={this.props.book.name}
+                                value={this.state.book.name}
                                 type="text"
                                 placeholder='Book name'
                                 id='name'
                                 name='name'
                                 className='form-control'
-                                onChange={this.props.handleChange}
+                                onChange={this.handleChange}
                             />
                         </div>
                         <div className='form-group'>
                             <input
-                                value={this.props.book.author}
+                                value={this.state.book.author}
                                 type="text"
                                 placeholder='Book author'
                                 id='author'
                                 name='author'
                                 className='form-control'
-                                onChange={this.props.handleChange}
+                                onChange={this.handleChange}
                             />
                         </div>
                         <div className='form-group'>
                         <textarea
-                            value={this.props.book.description}
+                            value={this.state.book.description}
                             placeholder='Book description'
                             id='description'
                             name='description'
                             className='form-control'
-                            onChange={this.props.handleChange}
+                            onChange={this.handleChange}
                         />
                         </div>
                         <div className='form-check'>
                             <input
-                                checked={this.props.book.bookOnStock}
+                                checked={this.state.book.bookOnStock}
                                 type="checkbox"
                                 id='bookOnStock'
                                 name='bookOnStock'
                                 className='form-check-input'
-                                onChange={this.props.handleChange}
+                                onChange={this.handleChange}
                             />
                             <label htmlFor="bookOnStock" className='form-check-label'>On stock</label>
                         </div>
                         <div className='form-group'>
                             <input
-                                value={this.props.book.image}
+                                value={this.state.book.image}
                                 type="text"
                                 placeholder='Book image'
                                 id='image'
                                 name='image'
                                 className='form-control'
-                                onChange={this.props.handleChange}
+                                onChange={this.handleChange}
                             />
                         </div>
                         <div className="form-group">
                             <label>
                                 Pick your favorite genre:
-                                <select name='select' value={this.props.book.select} onChange={this.props.handleChangeSelect}>
+                                <select name='select' value={this.state.book.select} onChange={this.handleChangeSelect}>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
@@ -73,7 +157,6 @@ class BookForm extends React.Component{
                             </label>
                         </div>
                         <button type='submit' className='btn btn-primary'>Add</button>
-                        <button type='button' className='btn btn-primary float-right' onClick={this.props.handleSignOut}>Log out</button>
                     </form>
                 </div>
             </div>
