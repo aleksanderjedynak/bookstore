3ef351cbe1250591fb587dbba24557516035e56e
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-09-23 21:06:33 +0200

    refactoring loginForm

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import { firebaseApp } from "../fbase";
 
 class LoginForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+
+        };
+    };
+
+    handleLoginChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    authenticate = (event) => {
+        console.log('[authenticate]',this.props);
+        event.preventDefault();
+        firebaseApp.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then( () => {
+                this.props.handleLoggedIn(true);
+                console.log("OK!!!");
+            } )
+            .catch( () => {
+                console.log('Unable to authenticate');
+            } );
+    };
 
     render(){
         return(
             <div className="row justify-content-center">
                 <div className="col-4">
-                    <form onSubmit={(event) =>(this.props.authenticate(event))}>
+                    <form onSubmit={(event) =>(this.authenticate(event))}>
                         <div className="form-group">
                             <label>Email address</label>
                             <input
@@ -15,8 +43,8 @@ class LoginForm extends React.Component{
                                 id='email'
                                 name='email'
                                 className='form-control'
-                                onChange={this.props.handleLoginChange}
-                                value={this.props.email}
+                                onChange={this.handleLoginChange}
+                                value={this.state.email}
                             />
                         </div>
                         <div className="form-group">
@@ -27,8 +55,8 @@ class LoginForm extends React.Component{
                                 name='password'
                                 className='form-control'
                                 placeholder='password'
-                                onChange={this.props.handleLoginChange}
-                                value={this.props.password}
+                                onChange={this.handleLoginChange}
+                                value={this.state.password}
                             />
                         </div>
                         <button type='submit' className='btn btn-secondary float-right'>log in</button>
